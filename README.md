4c96c39b7e4a75fa77b602f985fd4c004f00d62a
muhammad-tamim <contact2tamim@gmail.com>
2025-10-08 18:18:53 +0600

    chore: initial project setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Project Name: CS-Ticket System
+
+## Project Description:  
+
+## Live Site Link:
+
+## Project Video:
+
+## What I Learned New while Building This Project:
+
+## Challenges I faced while Building This Project:
+
+## Contact With Me: 
+
+tamim.muhammad2005@gmail.com | +8801586090360 (WhatsApp)  
+
+https://www.linkedin.com/in/tamim-muhammad
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+// main.jsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client';
+import './index.css'
+
+// Import React Router dependencies
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Welcome to the Home Page</h1>,
+
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
