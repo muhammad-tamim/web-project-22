863f1102f51aba413640e6fb966b1b83e3e81b1c
muhammad-tamim <contact2tamim@gmail.com>
2025-10-09 11:22:11 +0600

    fix(shared --> components --> ui --> buttons ):  secondary button pointer cursor not showing issue

--- src/shared/components/ui/buttons/SecondaryButton/SecondaryButton.jsx
+++ src/shared/components/ui/buttons/SecondaryButton/SecondaryButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SecondaryButton = ({ text }) => {
     return (
-        <button className='work-sans text-lg font-semibold text-white py-[18px] px-7 bg-secondary rounded-lg'>{text}</button>
+        <button className='work-sans cursor-pointer text-lg font-semibold text-white py-[18px] px-7 bg-secondary rounded-lg'>{text}</button>
     );
 };
 
