acd59b6596440fc1613a4ff009fbaf188e8f9db6
muhammad-tamim <contact2tamim@gmail.com>
2025-10-09 11:23:40 +0600

    feat: add container

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Navbar from '../../shared/components/structure/Navbar/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../../shared/components/structure/Footer/Footer';
+import Container from '../../shared/components/structure/Container/Container';
 
 const MainLayout = () => {
     return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
+        <Container>
+            <div>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+                <Footer></Footer>
+            </div>
+        </Container>
     );
 };
 
