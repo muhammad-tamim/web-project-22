f15cc332c8d2937ceb44ed1a409519493f37fe7e
muhammad-tamim <contact2tamim@gmail.com>
2025-10-09 11:02:29 +0600

    feat(shared --> components --> ui --> buttons ): add primary button

---
+++ src/shared/components/ui/buttons/PrimaryButton/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PrimaryButton = ({ text = "hello" }) => {
+    return (
+        <button className='work-sans text-lg font-semibold text-white py-[18px] px-7 bg-primary rounded-lg'>{text}</button>
+    );
+};
+
+export default PrimaryButton;
