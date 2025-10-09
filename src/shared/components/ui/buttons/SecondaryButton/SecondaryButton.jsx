c2a60068e405d17f87147f23dfa6b3889f736d74
muhammad-tamim <contact2tamim@gmail.com>
2025-10-09 11:02:44 +0600

    feat(shared --> components --> ui --> buttons ): add secondary button

---
+++ src/shared/components/ui/buttons/SecondaryButton/SecondaryButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SecondaryButton = ({ text }) => {
+    return (
+        <button className='work-sans text-lg font-semibold text-white py-[18px] px-7 bg-secondary rounded-lg'>{text}</button>
+    );
+};
+
+export default SecondaryButton;
