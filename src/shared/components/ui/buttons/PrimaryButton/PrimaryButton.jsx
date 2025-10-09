e49bf0c6c326e00948b93453423e28376b99b1c1
muhammad-tamim <contact2tamim@gmail.com>
2025-10-09 11:21:43 +0600

    fix(shared --> components --> ui --> buttons ):  primary button pointer cursor not showing issue

--- src/shared/components/ui/buttons/PrimaryButton/PrimaryButton.jsx
+++ src/shared/components/ui/buttons/PrimaryButton/PrimaryButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PrimaryButton = ({ text = "hello" }) => {
     return (
-        <button className='work-sans text-lg font-semibold text-white py-[18px] px-7 bg-primary rounded-lg'>{text}</button>
+        <button className='work-sans cursor-pointer text-lg font-semibold text-white py-[18px] px-7 bg-primary rounded-lg'>{text}</button>
     );
 };
 
